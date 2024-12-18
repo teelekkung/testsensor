3a1ae464426ebf42d14407857871f4f01bd185f2
Teelekkung <sahakiyat8680@gmail.com>
2024-12-19 00:14:48 +0700

    super fresh restart

---
+++ oldcode/old4/src/main.cpp
@@ -0,0 +1,37 @@
+#include "gheader.h"
+#include "gvalue.h"
+#include "gfuntion.h"
+
+unsigned long ploop1 = 0 ;
+const long loop1 = 20 ;
+unsigned long ploop2 = 0 ;
+const long loop2 = 2000 ;
+
+void setup() {
+    //put the code that run onece
+    Serial.begin(115200) ;
+    pinMode(TTP223, INPUT) ;
+    pinMode(TEMP6000, INPUT) ;
+    //setup i2crtc
+    mytwowireinit() ;
+    i2cdisplayinit() ;
+    i2crtcinit() ;
+    rgbstripinit() ;
+    aht10init() ;
+    bmp280init() ;
+}
+
+void loop() {
+    //put the code that run infinitly
+    if(millis() - ploop1 >= loop1) {
+        ploop1 = millis() ;
+        rgbstriprun() ;
+        pulseled();
+    }
+    if(millis() - ploop2 >= loop2) {
+        ploop2 = millis() ;
+        aht10run() ;
+        bmp280run() ;
+        i2crtcrun() ;
+    }
+}
